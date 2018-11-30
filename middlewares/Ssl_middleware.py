c068eb959e840bdd84e61ddda5248c9ad576b147
Zoltan Csontos <zoltancsontos@zoltans-mbp.attlocal.net>
2018-11-29 17:18:26 -0800

    Added extension exceptions

--- middlewares/Ssl_middleware.py
+++ middlewares/Ssl_middleware.py
@@ -7,13 +7,22 @@ class SslMiddleware(object):
     """
     Ssl forcing middleware
     """
+    EXTENSION_WHITELIST = [
+        '.css',
+        '.js',
+        '.png',
+        '.svg',
+        '.jpg',
+        '.ico'
+    ]
 
     def process_request(self, req, resp):
-        if SETTINGS['FORCE_SSL']:
-            print('ssl is here', req.url)
-            print(req.scheme)
-            if 'https' not in req.url:
-                print('http')
-                new_url = req.url.replace('http', 'https')
-                logging.debug(new_url)
-                raise falcon.HTTPTemporaryRedirect(new_url)
+        for item in self.EXTENSION_WHITELIST:
+            if SETTINGS['FORCE_SSL'] and item not in req.url:
+                print('ssl is here', req.url)
+                print(req.scheme)
+                if 'https' not in req.url:
+                    print('http')
+                    new_url = req.url.replace('http', 'https')
+                    logging.debug(new_url)
+                    raise falcon.HTTPTemporaryRedirect(new_url)
