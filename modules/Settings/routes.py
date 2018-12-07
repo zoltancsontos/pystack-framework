6898d5215ea06e54d32c16aab171d84f93444a03
Zoltan Csontos <Csontos_Zoltan@solarturbines.com>
2018-12-07 14:21:43 -0800

    Added cli build

--- modules/Settings/routes.py
+++
@@ -1,7 +0,0 @@
-from modules.Settings.Settings_resource import SettingsResource
-
-routes = [
-    # Settings api routes
-    {'url': '/v1/settings', 'controller': SettingsResource()},
-    {'url': '/v1/settings/{uid}', 'controller': SettingsResource()}
-]
--- modules/TimeSeries/routes.py
+++
@@ -1,9 +0,0 @@
-from modules.TimeSeries.TimeSeries_resource import TimeSeriesResource
-from modules.TimeSeries.TimeSeries_filter_resource import TimeSeriesFilterResource
-
-routes = [
-    # TimeSeries api routes
-    {'url': '/v1/time-data', 'controller': TimeSeriesResource()},
-    {'url': '/v1/time-data/{uid}', 'controller': TimeSeriesResource()},
-    {'url': '/v1/time-data/filter', 'controller': TimeSeriesFilterResource()}
-]
