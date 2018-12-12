7464cf3103b02a98969a2853ebc11b682a84fd17
Zoltan Csontos <zoltancsontos@eoa-10-222-45-17.eoa.cat.com>
2018-12-12 11:23:21 -0800

    Test setup

---
+++ tests/test_runner.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestRunner(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_numbers_3_4(self):
+        self.assertEqual(2, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
