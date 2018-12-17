20bf212f9198f53b7fb36fa7f074410ac27608af
Zoltan Csontos <zoltancsontos@eoa-10-222-38-230.eoa.cat.com>
2018-12-17 14:53:58 -0800

    Initial build prepared

--- modules/Todos/routes.py
+++
@@ -1,7 +0,0 @@
-from modules.Todos.Todos_resource import TodosResource
-
-routes = [
-    # Todos api routes
-    {'url': '/v1/todos', 'controller': TodosResource()},
-    {'url': '/v1/todos/{uid}', 'controller': TodosResource()}
-]
